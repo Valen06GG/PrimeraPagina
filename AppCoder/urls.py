907925166c70d0326dd0989120d6ee5c898b9947
Valentin Fortunato <valenfortunato06@gmail.com>
2025-06-12 15:40:11 -0300

    PrimeraPagina

--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -1,14 +1,13 @@
 from django.urls import path
 from .views import lista_estudiantes, detalle_estudiante, cursos, inicio, profesores, estudiante, entregable
 
-from AppCoder import views
 
 urlpatterns = [
     #path('estudiantes/', lista_estudiantes, name='lista_estudiantes'),
     #path('estudiantes/<int:pk>/', detalle_estudiante, name='detalle_estudiante'),
-    path('', views.inicio, name="inicio"),
-    path('cursos', views.cursos, name="cursos"),
-    path('profesores/', views.profesores, name="profesores"),
-    path('estudiante/', views.estudiante, name="estudiante"),
-    path('entregable/', views.entregable, name="entregable"),
+    path('', inicio, name="inicio"),
+    path('cursos', cursos, name="cursos"),
+    path('profesores/', profesores, name="profesores"),
+    path('estudiante/', estudiante, name="estudiante"),
+    path('entregable/', entregable, name="entregable"),
 ]
